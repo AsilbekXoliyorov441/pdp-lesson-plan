dfe2a8ab3d581085b77c1f47a7536efa99ef1aed
AsilbekXoliyorov441 <asilbekxoliyorov441@gmail.com>
2026-05-01 08:51:15 +0500

    adding changes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,9 +1,9 @@
-const LOGIN = "asilbek";
-const PASSWORD = "937550412";
+const LOGIN = "Teacher";
+const PASSWORD = "1234567890";
 
 export function login(username, password) {
   if (username === LOGIN && password === PASSWORD) {
-    localStorage.setItem("pdp-junior-token", "logged-in");
+    localStorage.setItem("pdp-junior-tok", "AccessEnter");
     return true;
   }
 
@@ -11,9 +11,9 @@ export function login(username, password) {
 }
 
 export function logout() {
-  localStorage.removeItem("pdp-junior-token");
+  localStorage.removeItem("pdp-junior-tok");
 }
 
 export function isLoggedIn() {
-  return Boolean(localStorage.getItem("pdp-junior-token"));
+  return Boolean(localStorage.getItem("pdp-junior-tok"));
 }
